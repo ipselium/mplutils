5d1c597ec3aa0ed3cb07037b2a653ecd43f03a1a
ipselium <ipselium@free.fr>
2019-05-07 19:25:42 +0200

    fix: Midpoint adjust if boundaries are not correct

--- mplutils/custom_cmap.py
+++ mplutils/custom_cmap.py
@@ -139,7 +139,12 @@ class MidpointNormalize(colors.Normalize):
         colors.Normalize.__init__(self, vmin, vmax, clip)
 
     def __call__(self, value, clip=None):
-        # I'm ignoring masked values and all kinds of edge cases to make a
-        # simple example...
-        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
-        return np.ma.masked_array(np.interp(value, x, y))
+
+        if self.vmin < self.midpoint < self.vmax:
+            x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+            return np.ma.masked_array(np.interp(value, x, y))
+
+        else:
+            x, y = [self.vmin, (self.vmax+self.vmin)/2, self.vmax], [0, 0.5, 1]
+            return np.ma.masked_array(np.interp(value, x, y))
+
